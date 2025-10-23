3a859aa510cb56d8759ff5c24cec343d3eca8ac8
maryna <maryna@cloudomation.com>
2025-10-23 13:56:12 +0200

    create Edit page

---
+++ app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,19 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface SnippetEditPageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+  const { id: idParam } = await props.params;
+  const id = parseInt(idParam);
+
+  const snippet = await db.snippet.findFirst({ where: { id } });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>Editing snippet</div>;
+}
--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import { db } from "@/db";
+import Link from "next/link";
 
 interface SnippetShowPageProps {
   params: Promise<{
@@ -23,7 +24,12 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
       <div className="flex m-4 justify-between items-center">
         <h1 className="text-xl font-bold">{snippet.title}</h1>
         <div className="flex gap-4">
-          <button className="p-2 border rounded">Edit</button>
+          <Link
+            className="p-2 border rounded"
+            href={`/snippets/${snippet.id}/edit`}
+          >
+            Edit
+          </Link>
           <button className="p-2 border rounded">Delete</button>
         </div>
       </div>
